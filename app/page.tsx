903406d4616d3bb66110f27d9550c8b80ea8e61f
saksham-tomer <sakshamtomerdevs@gmail.com>
2024-09-26 21:33:47 +0530

    landing done

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,10 @@ import Main2 from "@/components/3D/Main2";
 import Main3 from "@/components/3D/Main3";
 import MarqueeEffect from "@/components/3D/Marquee";
 import MarqueeEffect2 from "@/components/3D/Marquee2";
+import MarqueeEffect3 from "@/components/3D/Marquee3";
 import Experience from "@/components/3D/Scene4";
+import Card from "@/components/ui/Card";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
   return (
@@ -43,7 +46,63 @@ export default function Home() {
           This is beast studio
         </div>
       </div>
-      <div className="min-h-screen overflow-hidden min-w-full mt-[0rem] bg-neutral-800"></div>
+      <div className="min-h-screen relative flex-col flex items-center justify-center overflow-hidden min-w-full mt-[0rem] bg-neutral-800">
+        <MarqueeEffect3 />
+        <div className="grid grid-flow-row grid-col-1 md:grid-cols-3 gap-4">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+      <div className="flex bg-neutral-800 w-full p-4 flex-col md:flex-row">
+        <div className="flex flex-col md:flex-row justify-between w-full">
+          <div className="flex flex-col">
+            <h1 className="font-impact uppercase font-light text-white text-2xl">
+              A KID CALLED BEAST
+            </h1>
+            <p className=" text-sm text-white font-mono uppercase mt-2">
+              Join our mission and have your digital identity
+            </p>
+            <nav className="bg-neutral-800 text-white py-4 mt-10">
+              <ul className="flex justify-center space-x-8">
+                {["COMMUNITY", "CONTACT", "QUIZ", "BEASTHOOD"].map(
+                  (item, index) => (
+                    <li key={index} className="relative group">
+                      <a
+                        href="#"
+                        className="text-sm font-medium hover:text-gray-300 transition duration-150 ease-in-out"
+                      >
+                        {item}
+                        <span className="absolute bottom-0 left-0 w-full h-0.5 bg-white transform scale-x-0 group-hover:scale-x-100 transition-transform duration-150 ease-in-out"></span>
+                      </a>
+                      {index < 3 && (
+                        <span className="absolute top-1/2 -right-4 transform -translate-y-1/2 text-gray-500">
+                          ◼
+                        </span>
+                      )}
+                    </li>
+                  )
+                )}
+              </ul>
+            </nav>
+          </div>
+          <div className="gap-4">
+            <p className="font-impact text-white font-light text-sm uppercase">
+              Sign up for our latest news
+            </p>
+            <div className="relative gap-4">
+              <input
+                type="email"
+                placeholder="YOUR EMAIL"
+                className="w-full bg-transparent border-b font-mono border-white text-white placeholder-white py-2 pr-8 focus:outline-none focus:border-white"
+              />
+              <button className="absolute right-0 top-1/2 transform -translate-y-1/2 text-white">
+                <ArrowRight className="font-light text-white w-6 h-6" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- components/3D/Marquee3.tsx
+++ components/3D/Marquee3.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-const MarqueeEffect = () => {
+const MarqueeEffect3 = () => {
   return (
-    <div className="min-w-screen h-44 bg-orange-600 border-t-4 border-b-4  border-black top-[44rem] rotate-3 z-20 overflow-hidden relative">
+    <div className="min-w-screen h-44 bg-orange-600 border-t-4 border-b-4  border-black top-[33rem] rotate-3 z-20 overflow-hidden relative">
       <div className="absolute inset-0 m-2 p-4 min-h-9 transform-gpu  border-t-2 border-spacing-9 border-b-2 border-dashed border-black"></div>
       <div className="flex items-center h-full animate-marquee">
         {[...Array(10)].map((_, index) => (
@@ -18,4 +18,4 @@ const MarqueeEffect = () => {
   );
 };
 
-export default MarqueeEffect;
+export default MarqueeEffect3;
